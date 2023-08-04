610e09e1b815c56b3b79af381039ba46dbe0748d
santoshinautiyal <nautiyalsanto65@gmail.com>
2023-08-04 11:48:19 +0530

    new project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ('express')
+const app = express()
+const http =require('http').createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.get('/', (req,res)=>{
+    res.send('Hello world')
+})
